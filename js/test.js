2db5c53057ad7a524d43caf17df938d29ba8d09e
HoSeokMoon <adele007fall@naver.com>
2020-09-09 03:04:35 +0900

    node 스터디
    서버생성 및 게시물 작성 삭제 수정

---
+++ js/test.js
@@ -0,0 +1,157 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
+var templete = {
+    HTML:function(title, list, body, control){
+        return `<!doctype html>
+        <html>
+        <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+        </head >
+        <body>
+        <h1><a href="/">WEB</a></h1>
+        ${list}
+        ${control}
+        ${body}
+        </body>
+        </html> `
+    },
+    List:function(fileList){
+        var list = '<ul>';
+        list = list + '</ul>' 
+        var i = 0;
+        while (i < fileList.length){
+            list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`
+            i = i + 1;
+        } 
+        return list;
+    }
+} 
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query; 
+    var pathname = url.parse(_url, true).pathname;
+    if(pathname === '/'){ 
+        if(queryData.id === undefined){ 
+            fs.readdir(`./data`,function(err, fileList){ 
+                    var title = 'welcome';
+                    var description = 'hello node.js' 
+                    var list = templete.List(fileList)
+                    var HTML = templete.HTML(title, list, `<h2>${title}</h2>${description}`, 
+                    `<a href="/create">create</a> 
+                    <a href="/update?id=${title}">update</a>`); 
+                    response.writeHead(200);
+                    response.end(HTML);
+                });  
+        }else{
+            fs.readdir(`./data`,function(err, fileList){
+                var list = templete.List(fileList)
+            fs.readFile(`./data/${queryData.id}`,'utf8',
+            function(err, description){ 
+                var title = queryData.id;
+                var HTML = templete.HTML(title, list, `<h2>${title}</h2>${description}`, 
+                `
+                    <a href="/create">create</a>
+                    <a href="/update?id=${title}">update</a>
+                    <form action="/process_delete" method="post">
+                        <input type="hidden" name="id" value="${title}">
+                        <input type="submit" value="delete" >
+                    </form> 
+                `);
+                response.writeHead(200);
+                response.end(HTML);
+            });
+        });
+        }
+    }else if(pathname === '/create'){
+        fs.readdir(`./data`,function(err, fileList){ 
+            var title = 'create'; 
+            var list = templete.List(fileList)
+            var HTML = templete.HTML(title, list, `
+                <form action="/process_update" method="post">
+                    <p><input type="text" name="title" placeholder="title"></p>
+                    <p><textarea name="description" id="" cols="30" rows="10"></textarea></p>
+                    <p><input type="submit"></p>
+                </form>
+            `,''); 
+            response.writeHead(200);
+            response.end(HTML);
+        }); 
+    }else if(pathname === '/process_create'){
+        fs.readdir(`./data`,function(err, fileList){ 
+            var body = '';
+            request.on('data', function(data){
+                body += data;
+            });
+            request.on('end', function(){
+                var post = qs.parse(body);
+                var title = post.title;
+                var description = post.description; 
+                fs.writeFile(`./data/${title}`, description, 'utf-8', function(err){
+                    response.writeHead(302,{
+                        Location: `/?id=${title}`
+                    });
+                    response.end();  
+                })
+            });
+        }); 
+    }else if(pathname === '/update'){
+        fs.readdir(`./data`,function(err, fileList){
+            fs.readFile(`./data/${queryData.id}`,'utf-8',function(err, description){ 
+                var title = queryData.id; 
+                var list = templete.List(fileList)
+                var HTML = templete.HTML(title, list, `
+                    <form action="/process_update" method="post">
+                        <input type="hidden" name="id" value="${title}"></input>
+                        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                        <p>
+                            <textarea name="description" placeholder="description">${description}</textarea>
+                        </p>
+                        <p><input type="submit"></p>
+                    </form> 
+                `,`<a href="/create">create</a>
+                <a href="/update?id=${title}"">update</a>`); 
+                response.writeHead(200);
+                response.end(HTML);
+            });
+        });
+    }else if(pathname === '/process_update'){ 
+            var body = '';
+            request.on('data', function(data){
+                body += data;
+            });
+            request.on('end', function(){
+                var post = qs.parse(body);
+                var id = post.id;
+                var title = post.title;
+                var description = post.description; 
+                fs.rename(`./data/${id}`, `./data/${title}`, function(err){ 
+                    fs.writeFile(`./data/${title}`, description, 'utf-8', function(err){
+                        response.writeHead(302,{
+                            Location: `/?id=${title}`
+                        });
+                        response.end();  
+                    })
+                })
+            }); 
+    }else if(pathname === '/process_delete'){
+        var body = '';
+        request.on('data', function(data){
+            body += data;
+        });
+        request.on('end', function(){
+            var post = qs.parse(body);
+            var id = post.id;
+            fs.unlink(`./data/${id}`, function(err){
+                response.writeHead(302,{ Location: `/`});
+                response.end();  
+            })
+        }); 
+    }else{
+        response.writeHead(404);
+        response.end('not found');
+    } 
+});
+app.listen(3000)
